dd467b104d966a431cac13b748b064e8f3953390
Bennett Wineholt <bmw39@cornell.edu>
2023-11-10 03:02:02 -0500

    gcd z3 compare draft WIP, python compiling

--- README.md
+++ README.md
@@ -28,22 +28,36 @@ CS6120 Lesson 13 Program Synthesis
     - `git clone https://github.com/sampsyo/minisynth.git`
 
 - software
-    - `apt update && apt upgrade -y && apt install -y git vim wget`
-    - `apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev`
-    - `pwd # z3-source`
-    - Python 3.12 from source
-    - `cd /tmp`
-    - `wget https://www.python.org/ftp/python/3.12.0/Python-3.12.0.tgz`
-    - `tar -xzvf Python-3.12.0.tgz`
-    - `cd Python-3.12.0/`
-    - `./configure --enable-optimizations || ./configure --enable-optimizations`
-    - `make -j 2 `
+```
+apt update && apt upgrade -y && apt install -y git vim wget
+apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev
+pwd # z3-source
+# Python 3.12 from source
+cd /tmp
+wget https://www.python.org/ftp/python/3.12.0/Python-3.12.0.tgz
+tar -xzvf Python-3.12.0.tgz
+cd Python-3.12.0/
+./configure --enable-optimizations || ./configure --enable-optimizations
+echo $?  # want 0
+make -j 2 || make || make -j 2 || make -j 2
+echo $?  # want 0
 
-    - `cd /`
-    - `git clone https://github.com/rcplane/CS6120-L13-synthesis.git`
-    - `cd CS6120-L13-synthesis`
-    - `python gcd.py`
+make altinstall
+python3.12 -V
+# Python 3.12.0
 
+# ln -s /usr/local/bin/python3.12 /usr/local/bin/python
+# ls -al /usr/local/bin/python
+# python -VV
+# Python 3.12.0 ...
+
+
+# Run Z3 for GCD algorithm comparison by operation cost over a range of inputs
+cd /
+git clone https://github.com/rcplane/CS6120-L13-synthesis.git
+cd CS6120-L13-synthesis
+python gcd.py
+```
 
 
 
--- gcd.py
+++ gcd.py
@@ -1,3 +1,77 @@
-import z3
+from z3 import *
 
-# a stub
+# WIP
+
+def euclid(a, b, op_count):
+    # This is the 'hole' where different GCD strategies can be plugged in.
+    # For now, it simply represents the standard Euclidean algorithm.
+    new_a = If(a > b, a - b, a)
+    new_b = If(b > a, b - a, b)
+    new_op_count = op_count + 1
+    return new_a, new_b, new_op_count
+
+def stein(a, b, op_count):
+    # Implementing Stein's binary GCD algorithm
+    # Checking for base cases and applying identities
+    if a == 0:
+        return 0, b, op_count
+    elif b == 0:
+        return a, 0, op_count
+    else:
+        # Counting the operation
+        op_count += 1
+        # Making both numbers odd by removing all factors of 2
+        while a % 2 == 0:
+            a = a / 2
+            op_count += 1
+        while b % 2 == 0:
+            b = b / 2
+            op_count += 1
+        # Now applying the identity for odd numbers
+        if a > b:
+            a = a - b
+        else:
+            b = b - a
+    return a, b, op_count
+
+# Create an optimization solver
+opt = Optimize()
+
+# Define variables with tighter bounds
+a = Int('a')
+b = Int('b')
+gcd = Int('gcd')
+op_count = Int('op_count')  # Operation cost variable
+
+# Preconditions (e.g., a and b are positive)
+# Set tighter bounds, for example, values between 1 and 100
+opt.add(And(a > 0, a <= 100))
+opt.add(And(b > 0, b <= 100))
+
+# Initialize operation count
+opt.add(op_count == 0)
+
+# Loop invariant and operation count with Stein's algorithm hole
+while_condition = a != b
+
+def solve_for_alg(alg):
+    # Loop unrolling
+    for i in range(10):  # Arbitrary unroll limit for demonstration
+        a, b, op_count = alg(a, b, op_count)
+        opt.add(If(while_condition,
+                   And(gcd == GCD(a, b), a == a, b == b),
+                   And(gcd == GCD(a, b), a == a, b == b)))
+    # Minimize the operation count
+    opt.minimize(op_count)
+    # Check for the optimal solution
+    if opt.check() == sat:
+        print("Minimum operation count model:", opt.model())
+    else:
+        print("Problem not solvable.")
+
+def main():
+    solve_for_alg(euclid)
+    solve_for_alg(stein)
+
+if __name__ == "__main__":
+    main()
